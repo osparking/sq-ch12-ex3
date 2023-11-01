49342ed5744280e63a722bb95874d2dd2ae0921d
osparking <jbpark03@gmail.com>
2023-11-02 07:17:17 +0900

    구매정보 처리 두 REST 종점

---
+++ src/main/java/controller/PurchaseController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import sqch12ex3.domain.Purchase;
+import sqch12ex3.repository.PurchaseRepository;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/purchase")
+public class PurchaseController {
+	
+	private final PurchaseRepository repository;
+	
+	@PostMapping
+	public void createPurchase(@RequestBody Purchase purchase) {
+		repository.createPurchase(purchase);
+	}
+	
+	@GetMapping
+	public List<Purchase> findAllPurchases() {
+		return repository.getAllPurchases();
+	}
+}
