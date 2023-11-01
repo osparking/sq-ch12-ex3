b065a148f8094508502199dd78fa8fef27ede113
osparking <jbpark03@gmail.com>
2023-11-01 14:40:01 +0900

    디비 자료 원천에 대한 맞춤 설정

--- src/main/java/sqch12ex3/config/ProjectConfig.java
+++ src/main/java/sqch12ex3/config/ProjectConfig.java
@@ -2,6 +2,7 @@ package sqch12ex3.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,9 +11,22 @@ import com.zaxxer.hikari.HikariDataSource;
 @Configuration
 public class ProjectConfig {
 	
+	@Value("${custom.datasource.url}")
+	private String datasourceUrl;
+	
+	@Value("${custom.datasource.username}")
+	private String datasourceUsername;
+	
+	@Value("${custom.datasource.password}")
+	private String datasourcePassword;
+	
 	@Bean
 	DataSource customDataSource() {
 		HikariDataSource dataSource = new HikariDataSource();
+		dataSource.setJdbcUrl(datasourceUrl);
+		dataSource.setUsername(datasourceUsername);
+		dataSource.setPassword(datasourcePassword);
+		dataSource.setConnectionTimeout(1000);
 		return dataSource;
 	}
 
