60c28f8aa545787759c272a2656efe06527b6fee
osparking <jbpark03@gmail.com>
2023-11-01 13:03:34 +0900

    종속: web, lombok, jdbc, mariaDB

---
+++ src/main/java/sqch12ex3/SqCh12Ex3Application.java
@@ -0,0 +1,13 @@
+package sqch12ex3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh12Ex3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh12Ex3Application.class, args);
+	}
+
+}
---
+++ src/test/java/sqch12ex3/SqCh12Ex3ApplicationTests.java
@@ -0,0 +1,13 @@
+package sqch12ex3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh12Ex3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
