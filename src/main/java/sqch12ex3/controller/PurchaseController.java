5a73cac4a608bfdf31be78e558a3269ef7a38546
osparking <jbpark03@gmail.com>
2023-11-02 09:09:47 +0900

    controller action for a record deletion

--- src/main/java/sqch12ex3/controller/PurchaseController.java
+++ src/main/java/sqch12ex3/controller/PurchaseController.java
@@ -3,6 +3,7 @@ package sqch12ex3.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,11 @@ import sqch12ex3.repository.PurchaseRepository;
 public class PurchaseController {
 	
 	private final PurchaseRepository repository;
+	
+	@PostMapping("/{id}")
+	public void deletePurchase(@PathVariable int id) {
+		repository.deletePurchase(id);
+	}
 	
 	@PostMapping
 	public void createPurchase(@RequestBody Purchase purchase) {
